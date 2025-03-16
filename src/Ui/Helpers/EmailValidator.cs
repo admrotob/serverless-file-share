e971c8d9dc408081edeb4e7daeb5030165aa4db7
Andres Martinez <andres.martinezd@outlook.com>
2025-03-16 12:36:28 -0400

    initial commit

---
+++ src/Ui/Helpers/EmailValidator.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mail;
+
+namespace Ui.Helpers
+{
+    public static class EmailValidator
+    {
+        /// <summary>
+        /// Takes a given string and uses the System.Net.Mail API to validate that supplied input is a valid email.
+        /// </summary>
+        /// <param name="input">A string input to test a supplied email for validity.</param>
+        /// <returns>bool (true/false)</returns>
+        public static bool IsValidEmail(string input)
+        {
+            MailAddress result;
+            System.Net.Mail.MailAddress.TryCreate(input, out result);
+
+            if (result == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
